26b215d492477fcb2fc350b6ce87d0d6edb134d8
Devpratapsingh <95543075+Devpratapsingh@users.noreply.github.com>
2024-11-13 17:27:41 +0530

    Delete Frontend/amazon-app directory

--- Frontend/amazon-app/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Token from "./redux/slices/authSlice/token";
-export const store = configureStore({
-  reducer: {
-    auth: Token,
-  },
-});
